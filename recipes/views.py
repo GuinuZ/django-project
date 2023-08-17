2bc286f21a4f4fd2cc634a8d5847f854d57aee70
GuinuZ <shinigamizero2@gmail.com>
2023-08-17 13:43:32 -0300

    iniciando-projeto-real

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,10 +6,3 @@ from django.shortcuts import render
 def home(response):
     return render(response, 'recipes/home.html')
 
-
-def contato(response):
-    return HttpResponse("CONTATO")
-
-
-def sobre(response):
-    return HttpResponse("SOBRE")
